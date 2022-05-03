56d1ad7a9e2e739851c77174124a692a3915d03b
WillSkywalker <cxbats@gmail.com>
2022-05-04 01:23:06 +0200

    more gridsearch

--- run_lda_example.py
+++ run_lda_example.py
@@ -7,6 +7,7 @@ import numpy as np
 import pandas as pd
 from pprint import pprint
 import pandas
+from gensim.models import Phrases
 
 import file_handling as fh
 from scholar import Scholar
@@ -24,6 +25,7 @@ from sklearn.utils.estimator_checks import check_estimator
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
 
+from nltk.tokenize import RegexpTokenizer
 
 # compile some regexes
 punct_chars = list(set(string.punctuation) - set("'"))
@@ -34,27 +36,110 @@ alpha = re.compile('^[a-zA-Z_]+$')
 alpha_or_num = re.compile('^[a-zA-Z_]+|[0-9_]+$')
 alphanum = re.compile('^[a-zA-Z0-9_]+$')
 
+STOPWORD_LIST = fh.read_text(os.path.join('stopwords', 'nl_stopwords.txt'))
+STOPWORD_SET = {s.strip() for s in STOPWORD_LIST}
+
+T_R = RegexpTokenizer(r'\w+')
+
+zwanger = []
+with open('zwanger.txt', 'r') as f:
+    for line in f:
+        words = T_R.tokenize(line.lower())
+        words = [w for w in words if len(w) > 1]
+        zwanger.append(words)
+
+nzwanger = zwanger[:]
+zwanger_bigram = Phrases(nzwanger, min_count=2)
+for idx in range(len(nzwanger)):
+    for token in zwanger_bigram[nzwanger[idx]]:
+        if '_' in token:
+            nzwanger[idx].append(token)
+
+zwanger = [[w for w in t if w not in STOPWORD_SET] for t in zwanger]
+nzwanger = [[w for w in t if w not in STOPWORD_SET] for t in nzwanger]
+zwanger_dict = gensim.corpora.Dictionary(zwanger)
+zwanger_dict.filter_extremes(no_below=2, no_above=0.5)
+zwanger_dict.compactify()
+nzwanger_dict = gensim.corpora.Dictionary(nzwanger)
+nzwanger_dict.filter_extremes(no_below=2, no_above=0.5)
+nzwanger_dict.compactify()
+zwanger = [zwanger_dict.doc2bow(text) for text in zwanger]
+nzwanger = [nzwanger_dict.doc2bow(text) for text in nzwanger]
+
+
+kinderwens = []
+with open('kinderwens.txt', 'r') as f:
+    for line in f:
+        words = T_R.tokenize(line.lower())
+        words = [w for w in words if len(w) > 1]
+        kinderwens.append(words)
+
+nkinderwens = kinderwens[:]
+kind_bigram = Phrases(nkinderwens, min_count=2)
+for idx in range(len(nkinderwens)):
+    for token in kind_bigram[nkinderwens[idx]]:
+        if '_' in token:
+            nkinderwens[idx].append(token)
+
+kinderwens = [[w for w in t if w not in STOPWORD_SET] for t in kinderwens]
+nkinderwens = [[w for w in t if w not in STOPWORD_SET] for t in nkinderwens]
+
+kinderwens_dict = gensim.corpora.Dictionary(kinderwens)
+kinderwens_dict.filter_extremes(no_below=2, no_above=0.5)
+kinderwens_dict.compactify()
+kinderwens = [kinderwens_dict.doc2bow(text) for text in kinderwens]
+
+nkinderwens_dict = gensim.corpora.Dictionary(nkinderwens)
+nkinderwens_dict.filter_extremes(no_below=2, no_above=0.5)
+nkinderwens_dict.compactify()
+nkinderwens = [nkinderwens_dict.doc2bow(text) for text in nkinderwens]
 
 
 class LDAModel(BaseEstimator):
 
-    def __init__(self, K, alpha, beta, id2word, corpus, train_text):
+
+
+    def __init__(self, K, alpha, beta, corpuses, train_text, passes=1, iterations=50):
         self.lda_model = None
         self.K = K
         self.alpha = alpha
         self.beta = beta
-        self.id2word = id2word
-        self.corpus = corpus
+        self.corpuses = corpuses
+        self.id2word = corpuses[2]
+        self.corpus = corpuses[1]
+        self.input_corpus = corpuses[0]
         self.train_text = train_text
+        self.passes = passes
+        self.iterations = iterations
+
+        if 'ngram' in corpuses[0]:
+            self.zwanger = nzwanger
+            self.zwanger_dict = nzwanger_dict
+            self.kinderwens = nkinderwens
+            self.kinderwens_dict = nkinderwens_dict
+        else:
+            self.zwanger = zwanger
+            self.zwanger_dict = zwanger_dict
+            self.kinderwens = kinderwens
+            self.kinderwens_dict = kinderwens_dict
+
+
+
 
     def fit(self, x, Y=None):
-        print('Start training: K=%d, alpha=%s, beta=%s' % (self.K, self.alpha, self.beta))
+        self.id2word = self.corpuses[2]
+        self.corpus = self.corpuses[1]
+        self.input_corpus = self.corpuses[0]
+        x = self.corpus
+        print('Start training: K=%d, corpus=%s, alpha=%s, beta=%s, passes=%s' % (self.K, self.input_corpus,
+            self.alpha, self.beta, str(self.passes)))
         if self.alpha == 'auto' or self.beta == 'auto':
             self.lda_model = gensim.models.ldamodel.LdaModel(x, num_topics=self.K,
-                alpha=self.alpha, eta=self.beta, id2word=self.id2word)
+                alpha=self.alpha, eta=self.beta, id2word=self.id2word, passes=self.passes, iterations=self.iterations)
         else:
             self.lda_model = gensim.models.ldamulticore.LdaMulticore(x, 
-                num_topics=self.K, alpha=self.alpha, eta=self.beta, id2word=self.id2word, workers=16)
+                num_topics=self.K, alpha=self.alpha, eta=self.beta, 
+                id2word=self.id2word, passes=self.passes, iterations=self.iterations, workers=2)
 
     def transform(self, x):
         doc_topic_distr = []
@@ -70,13 +155,24 @@ class LDAModel(BaseEstimator):
         coherence_score = compute_npmi(self.lda_model, self.corpus, self.id2word)
         return coherence_score
 
+    def coherence_zwanger(self, x):
+        coherence_score = compute_npmi(self.lda_model, self.zwanger, self.zwanger_dict)
+        return coherence_score
+
+    def coherence_kinderwens(self, x):
+        coherence_score = compute_npmi(self.lda_model, self.kinderwens, self.kinderwens_dict)
+        return coherence_score
+
+
     def perplexity(self, x):
         return np.exp(-1. * self.lda_model.log_perplexity(x))
 
     def score(self, x, y=None):
-        print('Generating scores: K=%d, alpha=%s, beta=%s' % (self.K, self.alpha, self.beta))
+        print('Generating scores: K=%d, corpus=%s, alpha=%s, beta=%s' % (self.K, self.input_corpus,self.alpha, self.beta))
         scores = {'perplexity': self.perplexity(x),
-                'coherence': self.coherence(x)}
+                'coherence_internal': self.coherence(x),
+                'coherence_zwanger': self.coherence_zwanger(x),
+                'coherence_kinderwens': self.coherence_kinderwens(x)}
         pprint(scores)
         print('====================\n\n\n')
         return scores
@@ -144,6 +240,20 @@ def main(args):
     corpus = gensim.matutils.Sparse2Corpus(train_X)
 
     train_items = fh.read_jsonlist('data/kinderwens/all.jsonlist')
+    ngrams_text = [tokenize(t['text'])[0] for t in train_items]
+    bigram = Phrases(ngrams_text, min_count=2)
+    for idx in range(len(ngrams_text)):
+        for token in bigram[ngrams_text[idx]]:
+            if '_' in token:
+                ngrams_text[idx].append(token)
+    ngrams_text = [[w for w in t if w not in stopword_set] for t in ngrams_text]
+    id2word_ngram = gensim.corpora.Dictionary(ngrams_text)
+    id2word_ngram.filter_extremes(no_below=2, no_above=0.5)
+    id2word_ngram.compactify()
+    ngram_corpus = [id2word_ngram.doc2bow(text) for text in ngrams_text]
+    ngram_tfidf_model = gensim.models.TfidfModel(ngram_corpus)
+    ngram_tfidf_corpus = ngram_tfidf_model[ngram_corpus]
+
     train_text = [tokenize(t['text'], stopwords=stopword_set)[0] for t in train_items]
     train_texts = [clean_text(t['text']) for t in train_items]
     vectorizer = CountVectorizer(analyzer='word',       
@@ -151,49 +261,86 @@ def main(args):
                                  token_pattern='[a-zA-Z0-9]{2,}',  # num chars > 2
                                 )
 
-    data_vectorized = vectorizer.fit_transform(train_texts)
-
     id2word = gensim.corpora.Dictionary(train_text)
-    id2word.filter_extremes(no_below=2, no_above=0.4)
+    id2word.filter_extremes(no_below=2, no_above=0.5)
     id2word.compactify()
-    text_corpus = [id2word.doc2bow(text, allow_update=True) for text in train_text]
+    text_corpus = [id2word.doc2bow(text) for text in train_text]
     tfidf_model = gensim.models.TfidfModel(text_corpus)
     tfidf_corpus = tfidf_model[text_corpus]
 
 
-    dictionary = {i:w for i, w in enumerate(vocab)}
-    dictionary = gensim.corpora.Dictionary.from_corpus(corpus, id2word=dictionary)
+# ======= Q1
+
+    q1_train_items = fh.read_jsonlist('data/kinderwens_q1/all.jsonlist')
+    q1_ngrams_text = [tokenize(t['text'])[0] for t in q1_train_items]
+    q1_bigram = Phrases(q1_ngrams_text, min_count=2)
+    for idx in range(len(q1_ngrams_text)):
+        for token in q1_bigram[q1_ngrams_text[idx]]:
+            if '_' in token:
+                q1_ngrams_text[idx].append(token)
+    q1_ngrams_text = [[w for w in t if w not in stopword_set] for t in q1_ngrams_text]
+    q1_id2word_ngram = gensim.corpora.Dictionary(q1_ngrams_text)
+    q1_id2word_ngram.filter_extremes(no_below=2, no_above=0.5)
+    q1_id2word_ngram.compactify()
+    q1_ngram_corpus = [q1_id2word_ngram.doc2bow(text) for text in q1_ngrams_text]
+    q1_ngram_tfidf_model = gensim.models.TfidfModel(q1_ngram_corpus)
+    q1_ngram_tfidf_corpus = q1_ngram_tfidf_model[q1_ngram_corpus]
+
+    q1_train_text = [tokenize(t['text'], stopwords=stopword_set)[0] for t in q1_train_items]
+    q1_train_texts = [clean_text(t['text']) for t in q1_train_items]
+
+
+    q1_id2word = gensim.corpora.Dictionary(q1_train_text)
+    q1_id2word.filter_extremes(no_below=2, no_above=0.5)
+    q1_id2word.compactify()
+    q1_text_corpus = [q1_id2word.doc2bow(text) for text in q1_train_text]
+    q1_tfidf_model = gensim.models.TfidfModel(q1_text_corpus)
+    q1_tfidf_corpus = q1_tfidf_model[q1_text_corpus]
+
+
     # ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=options.n_topics, id2word=dictionary)
     # ldamodel = gensim.models.ldamodel.LdaModel(text_corpus, num_topics=options.n_topics, id2word=id2word)
 
-    search_params = {'K': list(range(3, 4)), 
-        # 'alpha': ['symmetric', 'auto', 0.1, 0.3, 0.5, 0.7, 0.9, 1, 2, 5],
-        # 'beta': ['symmetric', 'auto', 0.1, 0.3, 0.5, 0.7, 0.9, 1, 2, 5]
+    search_params = {'K': list(range(3, 13)), 
+        'alpha': ['symmetric', 'auto', 0.1, 0.3, 0.5, 0.7, 0.9, 1, 2, 5],
+        'beta': ['symmetric', 'auto', 0.1, 0.3, 0.5, 0.7, 0.9, 1, 2, 5],
+        'passes': [1, 2, 5, 10, 20, 50, 100],
+        'iterations': [50, 100, 200, 400],
+        'corpuses': [('all-original', text_corpus, id2word),
+                     ('all-tfidf', tfidf_corpus, id2word),
+                     ('all-original-ngrams', ngram_corpus, id2word_ngram),
+                     ('all-tfidf-ngrams', ngram_tfidf_corpus, id2word_ngram),
+                     ('q1-original', q1_text_corpus, q1_id2word),
+                     ('q1-tfidf', q1_tfidf_corpus, q1_id2word),
+                     ('q1-original-ngrams', q1_ngram_corpus, q1_id2word_ngram),
+                     ('q1-tfidf-ngrams', q1_ngram_tfidf_corpus, q1_id2word_ngram),]
+
     }
 
     test_corpus = gensim.matutils.Sparse2Corpus(test_X)
 
 
-    lda = LDAModel(3, 'symmetric', 'symmetric', id2word, corpus=text_corpus ,train_text=train_text)
-    lda1 = LDAModel(6, 0.3, 0.7, id2word, corpus=text_corpus ,train_text=train_text)
-    lda1.fit(text_corpus)
-    lda1.score(test_corpus)
-    lda2 = LDAModel(6, 'auto', 'auto', id2word, corpus=text_corpus ,train_text=train_text)
-    lda2.fit(text_corpus)
-    lda2.score(test_corpus)
+    lda = LDAModel(3, 'symmetric', 'symmetric', ('all-original', text_corpus, id2word),
+        train_text=train_text)
+    # lda1 = LDAModel(6, 0.3, 0.7, id2word, corpus=text_corpus ,train_text=train_text)
+    # lda1.fit(text_corpus)
+    # lda1.score(test_corpus)
+    # lda2 = LDAModel(6, 'auto', 'auto', id2word, corpus=text_corpus ,train_text=train_text)
+    # lda2.fit(text_corpus)
+    # lda2.score(test_corpus)
 
-    lda3 = LDAModel(6, 0.3, 0.7, id2word, corpus=tfidf_corpus,train_text=train_text)
-    lda3.fit(tfidf_corpus)
-    lda3.score(test_corpus)
-    lda4 = LDAModel(6, 'auto', 5, id2word, corpus=tfidf_corpus,train_text=train_text)
-    lda4.fit(tfidf_corpus)
-    lda4.score(test_corpus)
+    # lda3 = LDAModel(6, 0.3, 0.7, id2word, corpus=tfidf_corpus,train_text=train_text)
+    # lda3.fit(tfidf_corpus)
+    # lda3.score(test_corpus)
+    # lda4 = LDAModel(6, 'auto', 5, id2word, corpus=tfidf_corpus,train_text=train_text)
+    # lda4.fit(tfidf_corpus)
+    # lda4.score(test_corpus)
 
-    # grid_model = GridSearchCV(lda, param_grid=search_params, scoring=LDAModel.score, refit='perplexity')
-    # grid_model.fit(text_corpus)
+    grid_model = GridSearchCV(lda, param_grid=search_params, scoring=LDAModel.score, refit='perplexity')
+    grid_model.fit(text_corpus)
     # pprint(grid_model.score(test_corpus))
 
-    # pandas.DataFrame(grid_model.cv_results_).to_csv('LDA_gridsearch.csv')
+    pandas.DataFrame(grid_model.cv_results_).to_csv('LDA_gridsearch_0522.csv')
 
 
 
@@ -329,8 +476,9 @@ def compute_npmi(model, corpus, id2word, n=10):
 
     npmi_means = []
     for idx in range(model.num_topics):
-        words = [x[0] for x in model.get_topic_terms(idx)]
+        words = [id2word.token2id[x[0]] for x in model.show_topic(idx)]
         npmi_vals = []
+
         for word_i, word1 in enumerate(words[:n]):
             for word2 in words[word_i+1:n]:
 
